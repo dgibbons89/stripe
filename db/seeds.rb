678ca05486159b054cad3bfd12ede83a7cb14728
Dean Gibbons <dgibbons89@gmail.com>
2014-11-25 17:14:18 -0500

    added growth hacking course to seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+growth_hacking = Product.create(title: "Growth Hacking", price: "4.99", subtitle: "Crash Course", sku: "GROHACK1" , author: "Mattan Griffel", description: %{     <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+            <p><strong>What You'll Learn</strong></p>
+            <ul class="no-indent">
+                <li>What is a growth hacker?</li>
+                <li>The 5 stages of the user lifecycle</li>
+                <li>How to apply the Lean Marketing Framework</li>
+                <li>Resources and tools you'll need to know</li>
+            </ul>},)
