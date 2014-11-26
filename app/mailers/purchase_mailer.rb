e99b71d612a937ee79a5fb34a511aa5e9ac20a29
Dean Gibbons <dgibbons89@gmail.com>
2014-11-26 12:35:36 -0500

    reworked views to use product information from database

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,13 +1,10 @@
 class PurchaseMailer < ActionMailer::Base
-layout 'purchase_mailer'
+	layout 'purchase_mailer'
+	default from: "One Month Commerce <commerce@onemonth.com>"
 
- default from: "One Month Commerce <commerce@onemonth.com>"
-
- def purchase_receipt purchase
- @purchase = purchase
-
- mail to: purchase.email, subject: "Thanks for your purchase!"
-
-  end
-
+	def purchase_receipt purchase
+ 		@purchase = purchase
+ 		@product = Product.find(purchase.product_id)
+ 		mail to: purchase.email, subject: "Thanks for your purchase!"
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125214453) do
+ActiveRecord::Schema.define(version: 20141125224017) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,6 +45,20 @@ ActiveRecord::Schema.define(version: 20141125214453) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "author"
+    t.text     "description"
+    t.string   "sku"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "length"
+    t.text     "author_description"
+    t.string   "author_image_name"
+  end
 
   create_table "purchases", force: true do |t|
     t.string   "email"
